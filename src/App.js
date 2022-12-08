999e054f92a4c0057afc341b775dda51b51f469e
Bajrang Agrawal <bajrangofficial580@gmail.com>
2022-12-08 23:53:37 +0530

    [added] Login Page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 import './App.css';
 import Login from './components/Login';
+import Header from './components/Header';
+import Home from './components/Home';
 
 function App() {
   return (
@@ -10,6 +12,11 @@ function App() {
         <Switch>
           <Route exact path = "/">
             <Login />
+          </Route>
+          <Route exact path ="/home">
+            <Header/>
+            <Home/>
+
           </Route>
         </Switch>
       </Router>
