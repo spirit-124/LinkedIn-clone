83e6b892b443fbfad9c13039d01fea369425bd14
Bajrang Agrawal <bajrangofficial580@gmail.com>
2022-12-11 23:24:57 +0530

    [modified] HomePage

--- src/components/LeftSide.js
+++ src/components/LeftSide.js
@@ -6,7 +6,7 @@ const LeftSide = (props) => {
         <Container>
             <ArtCard>
                 <UserInfo>
-                    <CardBackground>
+                    <CardBackground />
                         <a>
                         <Photo/>
                         <Link> Welcome, there! </Link>
@@ -16,9 +16,41 @@ const LeftSide = (props) => {
                                 Add a Photo
                             </AddPhotoText>
                         </a>
-                    </CardBackground>
                 </UserInfo>
+                <Widget>
+                    <a>
+                        <div>
+                        <span>Connections</span>
+                        <span>Grow Your network</span>
+                        </div>
+                        <img src="/images/widget-icon.svg" alt="" />
+                    </a>
+                </Widget>
+                <Item>
+                    <span>
+                        <img src="/images/item-icon.svg" alt="" />
+                        My Items
+                    </span>
+                </Item>
             </ArtCard>
+            
+            <CommunityCard>
+                <a>
+                    <span>Groups</span>
+                </a>
+                <a>
+                    <span>
+                        Events
+                        <img src="/images/plus-icon.svg" alt="" />
+                    </span>
+                </a>
+                <a>
+                    <span>Follow HashTags</span>
+                </a>
+                <a>
+                    <span>Discover More</span>
+                </a>
+            </CommunityCard>
         </Container>
    
    );
@@ -65,9 +97,124 @@ const Photo = styled.div`
     background-clip: content-box;
     background-color: white;
     background-position: center;
+    background-size: 60%;
+    background-repeat: no-repeat;
+    border: 2px solid white;
+    margin: -38px auto 12px;
+    border-radius: 50%;
 `;
 
-const Link = styled.div``;
+const Link = styled.div`
+    font-size: 16px;
+    line-height: 1.5;
+    color: rgba(0,0,0,0.9);
+    font-weight: 600;
+`;
 
-const AddPhotoText = styled.div``;
+const AddPhotoText = styled.div`
+    color: #0a66c2;
+    font-size: 12px;
+    line-height: 1.33;
+    margin-top: 4px;
+    font-weight: 400;
+`;
+
+const Widget = styled.div`
+    border-bottom: 1px solid rgba(0,0,0,0.15);
+    padding-top: 12px;
+    padding-bottom: 12px;
+
+    & > a {
+        text-decoration: none;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 4px 12px;
+
+        &:hover {
+            background-color: rgba(0,0,0,0.08);
+        }
+
+        div {
+            display: flex;
+            flex-direction: column;
+            text-align: left;
+
+            span{
+                font-size: 12px;
+                line-height: 1.33;
+                &:first-child{
+                    color: rgba(0,0,0,0.6);
+                }
+                &:nth-child(2) {
+                    color: rgba(0,0,0,1);
+                }
+            }
+        }
+    }
+
+    svg{
+        color: rgba(0,0,0,1)
+    }
+`;
+
+const Item = styled.a`
+    border-color: rgba(0, 0, 0, 0.8);
+    text-align: left;
+    padding: 12px;
+    font-size: 12px;
+    display: block;
+
+    span {
+    display: flex;
+    align-items: center;
+    color: rgba(0, 0, 0, 1);
+
+    svg {
+        color: rgba(0, 0, 0, 0.6);
+    }
+
+    }
+
+    &:hover {
+        background-color: rgba(0, 0, 0, 0.08);
+    }
+
+`;
+
+const CommunityCard = styled(ArtCard)`
+    padding: 8px 0 0;
+    text-align: left;
+    display: flex;
+    flex-direction: column;
+    a {
+        color: black
+        padding 4px 12px 4px 12px;
+        font-size: 12px;
+        /* vertical-align: super; */
+
+        &:hover {
+            color: #0a66c2
+        }
+
+        span {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+
+        }
+        &:last-child {
+            color: rgba(0, 0, 0, 0.6);
+            text-decoration: none;
+            border-top: 1px solid  #d6cec2;
+            padding: 12px;
+
+            &:hover {
+                background-color: rgba(0, 0, 0, 0.08);
+            }
+        }
+    }
+`;
+
+
 export default LeftSide;
